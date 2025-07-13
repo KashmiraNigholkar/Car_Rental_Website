884f98623ce0a21569a59c8404642f7709d52f28
kashmira <kashmiranigholkar421@gmail.com>
2025-07-13 15:39:31 +0530

    second commit

--- CARRENTAL/client/src/App.jsx
+++ CARRENTAL/client/src/App.jsx
@@ -11,6 +11,7 @@ import DashBoard from './pages/owner/DashBoard';
 import AddCar from './pages/owner/AddCar';
 import ManageCars from './pages/owner/ManageCars';
 import ManageBookings from './pages/owner/ManageBookings';
+import Login from './components/Login';
 
 
 const App = () => {
@@ -18,7 +19,8 @@ const App = () => {
   const isOwnerPath = useLocation().pathname.startsWith('/owner');
 
   return (
-    <>
+    <>{showLogin && <Login setShowLogin={setShowLogin}/>}
+    
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
       <Routes>
